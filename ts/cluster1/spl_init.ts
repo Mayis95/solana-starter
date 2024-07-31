6e1dd5e5bc0340b6eaffccbd7b51398826ac1557
Mayis95 <aramayisorgov95@gmail.com>
2024-07-31 15:07:26 +0400

    Have finished SPL init and SPL mint

--- ts/cluster1/spl_init.ts
+++ ts/cluster1/spl_init.ts
@@ -1,6 +1,6 @@
 import { Keypair, Connection, Commitment } from "@solana/web3.js";
 import { createMint } from '@solana/spl-token';
-import wallet from "../wba-wallet.json"
+import wallet from "./wallet/wba-wallet.json"
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -12,8 +12,17 @@ const connection = new Connection("https://api.devnet.solana.com", commitment);
 (async () => {
     try {
         // Start here
-        // const mint = ???
+        const mint = await createMint(
+            connection,
+            keypair,
+            keypair.publicKey,
+            null,
+            6
+        );
+        console.log(`Succesfully created a new mint. Address: ${mint}`);
     } catch(error) {
         console.log(`Oops, something went wrong: ${error}`)
     }
 })()
+
+// mint address: CRcuBCxZ1FAhQfaYvEhys9jeHZbR3z1rVcVXN1VnVsNB
